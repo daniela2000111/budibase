c183fe59911c834918fbd658868c369d31e59210
Adria Navarro <adria@budibase.com>
2024-10-03 16:06:30 +0200

    Add basic CreateViewRequest

--- packages/types/src/api/web/app/view.ts
+++ packages/types/src/api/web/app/view.ts
@@ -1,3 +1,4 @@
+import { z } from "zod"
 import { ViewV2 } from "../../../documents"
 import { ViewV2Enriched } from "../../../sdk/view"
 
@@ -9,6 +10,16 @@ export interface ViewResponseEnriched {
   data: ViewV2Enriched
 }
 
-export interface CreateViewRequest extends Omit<ViewV2, "version" | "id"> {}
+// export interface CreateViewRequest extends Omit<ViewV2, "version" | "id"> {}
+
+const view = z.object({
+  name: z.string(),
+  tableId: z.string(),
+  query: z.any(),
+  sort: z.any(),
+  primaryDisplay: z.string(),
+})
+
+export type CreateViewRequest = z.infer<typeof view>
 
 export interface UpdateViewRequest extends ViewV2 {}
